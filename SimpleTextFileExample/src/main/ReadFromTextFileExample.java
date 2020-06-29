a3c27f2a327a888ece465f51f7be2909ce7ec5c6
Admin <Admin@DESKTOP-83356PE>
2020-06-29 17:55:24 +0300

    First Example

--- SimpleTextFileExample/src/main/ReadFromTextFileExample.java
+++ SimpleTextFileExample/src/main/ReadFromTextFileExample.java
@@ -1,9 +1,30 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadFromTextFileExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		File file = new File("data/person.txt");
+		
+		try {
+			Scanner in = new Scanner(file);
+			String fname;
+			String lname;
+			int age;
+			
+			fname = in.next();
+			lname = in.next();
+			age = in.nextInt();
+			System.out.println(fname + " " + lname +" " + age );
+		} catch (FileNotFoundException e) {
+			System.out.println("Nu pot citi!!!");
+		}
+		
+		
 
 	}
 
